1ca2b4d88e955ba8b7c965cf0abee0f70305ede4
lethaithanhson <lethaithanhson018@gmail.com>
2021-03-30 22:27:09 +0700

    up content

--- templates/content.php
+++ templates/content.php
@@ -1,1 +1,401 @@
-<p>đây là content</p>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/content.css">
+</head>
+
+
+<main class="site-main">
+  <!-- =========================================================== -->
+  <!-- 					        	CAROUSEL 			                  				 -->
+  <!-- =========================================================== -->
+
+  <div class="carousel-top">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="img-carousel" style="background-image: url('./img/carousel/home_01.jpg');">
+            <div class="carousel-content">
+              <h2>wellcom to 3ks petshop</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
+              <button type="button" class="btn button button-primary">Success</button>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="img-carousel" style="background-image: url('./img/carousel/home_02.jpg');">
+            <div class="carousel-content">
+              <h2>Where beging a pet is just the best</h2>
+              <p>Say hi to our Shop, your new onlinepal packer with on the element your website can ever possibity need</p>
+              <button type="button" class="btn button button-primary">Success</button>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="img-carousel" style="background-image: url('./img/carousel/home_03.jpg');">
+            <div class="carousel-content">
+              <h2>Where beging a pet is just the best</h2>
+              <p>Say hi to our Shop, your new onlinepal packer with on the element your website can ever possibity need.</p>
+              <button type="button" class="btn button button-primary">Success</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true">
+          <i class="fa fa-angle-left" aria-hidden="true"></i>
+        </span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true">
+          <i class="fa fa-angle-right" aria-hidden="true"></i>
+        </span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+  <!-- ======================================= -->
+  <!--                 BANNER                  -->
+  <!-- ======================================= -->
+  <div class="row banner">
+    <img src="./img/banner/banner.jpg" alt="" class="img-banner">
+    <div class="col-5 banner-content">
+      <h3>Sale up to 10%</h3>
+      <h1 class="banner-title">Accesories For Pet<span class="dots">.</span></h1>
+      <p>Make Your Pet's Day With Our Raange Of Pet accesories atPetoria today. Our Pet Bandanas are hanmade to the quality for your pet!</p>
+      <button type="button" class="btn button button-primary">Shop Now</button>
+    </div>
+  </div>
+
+  <!-- ======================================= -->
+  <!--            SERVICES SECTION             -->
+  <!-- ======================================= -->
+  <div class="services-section">
+    <div class="container">
+      <div class="content-title">
+        <h1>What we offer</h1>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-01.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Groming</h3>
+              <p>Let your pets enjoy the first-class grooming services provided by our certified and experienced groomers.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-02.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Pet hotel</h3>
+              <p>At Puffles, we offer a safe & clean environment with playtime, exercises, healthy meals & more.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-03.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Dog daycare</h3>
+              <p>Our daytime guests enjoy playtime with other dogs in a safe and fun environment.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-04.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Dog training</h3>
+              <p>Our accredited pet trainers teach fun & eﬀective group & private classes through positive reinforcement.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-05.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Veterinary care</h3>
+              <p>Save your pet the discomfort of advanced illness & help save you the stress & expense of treatment.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4">
+          <div class="services-box">
+            <div class="col-3 box-left">
+              <div class="services-icon" style="background-image: url('./img/services/icon-06.png');"></div>
+            </div>
+            <div class="col-9 box-body">
+              <h3>Adoption</h3>
+              <p>Whether you adopt online or at a Puffles store near you, you’re helping change the life of a pet in need.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <hr>
+    </div>
+  </div>
+  <!-- ======================================= -->
+  <!--    CAROUSEL CARD (NEW PRODUCT)          -->
+  <!-- ======================================= -->
+
+  <div class="card-carousel">
+    <div class="container">
+      <div class="content-title">
+        <h1>New product</h1>
+        <p>Ut enim ad minim veniam.</p>
+      </div>
+
+      <div id="pd-carousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#pd-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#pd-carousel" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="row">
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="row">
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+              <div class="col-4 card">
+                <a href="" class="card-link">
+                  <div class="card-img">
+                    <img src="https://timesofindia.indiatimes.com/photo/67586673.cms" alt="product" class="card-img-top">
+                  </div>
+                  <div class="card-body">
+                    <h2 class="card-title">Dog</h2>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="product-price">200.000 <sup>đ</sup></div>
+                    <button type="button" class="btn button">Add To Cart</button>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>   
+
+  <div class="banner justify-content-center">
+    <img src="./img/banner/banner-02.jpg" alt="" class="w-100">
+    <div class="banner-content position-absolute text-center p-3 w-75" style="color: var(--text-color);">
+      <h1>Pet food and toys</h1>
+      <p>Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button type="button" class="btn button button-primary">Shop Now</button>
+    </div>
+  </div>
+
+<!-- ======================================= -->
+<!--             ABOUT SHOP                  -->
+<!-- ======================================= -->
+
+  <div class="menu-section">
+
+    <div class="menu-box pets-box">
+      <div class="container">
+        <div class="row">
+          <div class="col-5">
+            <div class="img-holder">
+              <img src="./img/content/menu-content.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-6 offset-1">
+            <div class="menu-holder">
+              <h2 class="menu-title">Pets<span class="dots">.</span></h2>
+              <ul class="menu-list">
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Cat & Kitten</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Dog & Puppy</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Bird</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Snake</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+              </ul>
+              <button type="button" class="ntn button button-primary">See more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="menu-box service-box">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="menu-holder">
+              <h2 class="menu-title">Shop Services<span class="dots">.</span></h2>
+              <ul class="menu-list">
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Groming</h3>
+                  <p>Let your pets enjoy the first-class grooming services provided by our certified and experienced groomers.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Veterinary care</h3>
+                  <p>Save your pet the discomfort of advanced illness & help save you the stress & expense of treatment.
+                  </p>
+                </li>
+              </ul>
+              <button type="button" class="ntn button button-primary">See more</button>
+            </div>
+          </div>
+          <div class="col-5 offset-1">
+            <div class="img-holder">
+              <img src="./img/content/menu-content-01.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+    <div class="menu-box other-box">
+      <div class="container">
+        <div class="row">
+          <div class="col-5">
+            <div class="img-holder">
+              <img src="./img/content/menu-content-02.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-6 offset-1">
+            <div class="menu-holder">
+              <h2 class="menu-title">Items for pets<span class="dots">.</span></h2>
+              <ul class="menu-list">
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Cat & Kitten</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Dog & Puppy</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Bird</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+                <li class="menu-item">
+                  <h3><i class="fa fa-paw" aria-hidden="true"></i>Snake</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                  </p>
+                </li>
+              </ul>
+              <button type="button" class="ntn button button-primary">See more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</main>
